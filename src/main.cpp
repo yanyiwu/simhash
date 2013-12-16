c1996772b40a1f7cc108f23847e03b30ecd8efb8
wyy <wuyanyi09@gmail.com>
2013-12-16 03:48:39 -0800

    ci

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include "CppJieba/Limonp/io_functs.hpp"
+
+using namespace Limonp;
 
 
-int main()
+bool cut
+
+int main(int argc, char** argv)
 {
+    if(argc < 2)
+    {
+        fprintf(stderr, "usage: %s <filename>", argv[0]);
+        return EXIT_FAILURE;
+    }
+    string doc;
+    loadFile2Str(argv[0], doc);
     return EXIT_SUCCESS;
 }
