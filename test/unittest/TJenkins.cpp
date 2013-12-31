fdf43d50640581928132eefc8077085342e55481
wyy <wuyanyi09@gmail.com>
2013-12-30 19:30:45 -0800

    modify unittest

--- test/unittest/TJenkins.cpp
+++ test/unittest/TJenkins.cpp
@@ -15,6 +15,7 @@ TEST(JenkinsTest, Test1)
     string doc((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
     //cout<< doc<<endl;
     ASSERT_EQ(uint64_t(13697483439902700582U), hasher(doc.c_str(), doc.size(), 0));
+    ASSERT_EQ(uint64_t(13697483439902700582U), hasher(doc.c_str(), doc.size(), 0));
 }
 
 
