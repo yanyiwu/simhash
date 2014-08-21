1686220c6192d1cfaaac2425e939f045d68e8dd9
wyy <wuyanyi09@gmail.com>
2014-08-22 00:50:58 +0800

    compatible with mac

--- test/unittest/TJenkins.cpp
+++ test/unittest/TJenkins.cpp
@@ -11,7 +11,6 @@ TEST(JenkinsTest, Test1)
 {
     jenkins hasher;
     ifstream ifs("../test/testdata/news_content");
-    ASSERT_TRUE(ifs);
     string doc((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
     //cout<< doc<<endl;
     ASSERT_EQ(uint64_t(13697483439902700582U), hasher(doc.c_str(), doc.size(), 0));
